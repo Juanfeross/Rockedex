40c2ea56b8f8cfe094e3f4f5d018a451063d319b
Juan Fernando Álvarez Gallego <84760764+Juanfeross@users.noreply.github.com>
2022-11-12 18:26:47 -0500

    pkmn list done!!

---
+++ src/app/pages/pkm-list/pkm-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { PkmService } from '../../shared/services/pkm-service/pkm.service';
+import { IPokemon } from '../../shared/interfaces/pkm-interface/pkm-interface';
+import { debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-pkm-list',
+  templateUrl: './pkm-list.component.html',
+  styleUrls: ['./pkm-list.component.scss']
+})
+export class PkmListComponent implements OnInit {
+public addPkm: IPokemon[] = [];
+public tempAddPkm: IPokemon[] = [];
+
+  constructor(private pkmService: PkmService) { }
+
+  ngOnInit(): void {
+    this.pkmService.getPokemon().subscribe(
+      response => {
+        response.results.forEach(element => {
+          this.getPkmDetail(element.url);
+        });
+      }
+    )
+  }
+
+  private getPkmDetail(url: string = '') {
+    this.pkmService.getPokemonId(url, false).subscribe(
+      response => {
+        this.tempAddPkm.push(response);
+        this.addPkm = this.getPkmOrder();
+        console.log(this.getPkmOrder());
+      }
+    )
+  }
+
+  private getPkmOrder(): IPokemon[] {
+    const list = this.tempAddPkm.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      if (a.id < b.id) {
+        return -1;
+      }
+      return 0;
+    });
+    return list;
+  }
+}
